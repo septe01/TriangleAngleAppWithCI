18693b84181edf3347d4f6ee11bbcad700cc9e6f
septe habudin <septe.vhirgo@gmail.com>
2022-12-20 16:41:03 +0700

    initial commit testing integration ci/cd

---
+++ Triangle App/Triangle_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Triangle_AppApp.swift
+//  Triangle App
+//
+//  Created by septe habudin on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Triangle_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
