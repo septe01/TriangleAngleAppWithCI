27d20b155a6f04f589894d39959f515e153b2e1f
septe habudin <septe.vhirgo@gmail.com>
2022-12-20 17:01:57 +0700

    move to here

--- Triangle AppTests/Triangle_AppTests.swift
+++ Triangle AppTests/Triangle_AppTests.swift
@@ -10,27 +10,75 @@ import XCTest
 
 final class Triangle_AppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testInvalidInputSides() {
+        XCTAssertThrowsError(try detectTriangle(-2, 2, 3)) { error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
         }
     }
 
+    // cek is triangle Segitiga Sama Sisi
+    func testDetectEquilateralTriangle() {
+        XCTAssertEqual(try detectTriangle(2, 2, 2), "Segitiga Sama Sisi")
+    }
+
+    // test segitiga sama kaki
+    func testDetectIsoscelesTriangle() {
+        //        XCTAssertEqual(try detectTriangle(1, 1, 2), "Segitiga Sama Kaki") // jika di open akan masuk ke case pertama
+        //        XCTAssertEqual(try detectTriangle(1, 2, 1), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(8, 8, 10), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(8, 10, 8), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(1, 2, 2), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(2, 1, 2), "Segitiga Sama Kaki")
+    }
+
+    // test segitiga sembarang
+    func testDetectRandomTriangle() {
+        //        XCTAssertEqual(try detectTriangle(1, 2, 3), "Segitiga Sembarang")
+        XCTAssertEqual(try detectTriangle(3, 4, 5), "Segitiga Sembarang")
+    }
+
+    func testInequalityTriangle() {
+        XCTAssertThrowsError(try detectTriangle(4, 1, 2)) { error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.inequalityInput)
+        }
+
+        XCTAssertThrowsError(try detectTriangle(5, 1, 3)) {error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.inequalityInput)
+        }
+    }
+
+    func detectTriangle(
+        _ SideA: Int,
+        _ SideB: Int,
+        _ SideC: Int
+    ) throws -> String {
+        // sample hanya cek satu argumen saja
+        //        if SideA < 1 {
+        //            throw TriangleError.invalidInput
+        //        }
+
+        let sides = [SideA, SideB, SideC].sorted()
+
+        //        for side in [SideA, SideB, SideC] where side < 1 {
+        for side in sides where side < 1 {
+            throw TriangleError.invalidInput
+        }
+
+        if (sides[1] + sides[0] <= sides[2]) {
+            throw TriangleError.inequalityInput
+        } else if sides[0] == sides[1] && sides[1] == sides[2] {
+            return "Segitiga Sama Sisi"
+        } else if sides[0] == sides[1] || sides[1] == sides[2] {
+            return "Segitiga Sama Kaki"
+        } else {
+            return "Segitiga Sembarang"
+        }
+    }
+
+}
+
+
+enum TriangleError: Error {
+    case invalidInput
+    case inequalityInput
 }
